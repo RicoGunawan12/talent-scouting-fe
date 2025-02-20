82facee4244ae2b4005a342f830a28148e88ac77
Rico <ricogunawan12323@gmail.com>
2025-02-20 23:56:40 +0700

    update: fix vacancy apply

--- src/page/component/AppliedRequest.tsx
+++ src/page/component/AppliedRequest.tsx
@@ -33,9 +33,9 @@ const AppliedRequest = forwardRef<HTMLDivElement, StudentRequestProps>(
             <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-sm font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
               Waiting
             </span>
-          ) : status === "Approved to Interview" ? (
+          ) : status === "Accepted" ? (
             <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-sm font-medium text-green-700 ring-1 ring-inset ring-green-700/10">
-              Approved to Interview
+              Accepted
             </span>
           ) : (
             <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-sm font-medium text-red-700 ring-1 ring-inset ring-red-700/10">
